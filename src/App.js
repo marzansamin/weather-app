f925d8691c243e12edd5827d9da3ddf56c860167
marzansamin <msaminashrafi@gmail.com>
2024-05-06 18:33:35 +0600

    Done weather app using api

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WeatherApp from './Components/Weather App/WeatherApp';
+
+function App() {
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
